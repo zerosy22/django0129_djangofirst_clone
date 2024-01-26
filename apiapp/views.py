391559e836aacbffa6520057109b8d7e9e45cc6c
adam <itstudy@kakao.com>
2024-01-26 15:10:56 +0900

    second

--- apiapp/views.py
+++ apiapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view 
 
-# Create your views here.
+#python에서 @으로 시작하는 단어는 decorator 라고 하는데 실제 함수를 호출하면
+#특정 내용을 삽입해서 함수를 실행합니다.
+#반복적으로 사용하는 내용이나 직접 작성하기 번거로운 내용을 decorator로 만듭니다.
+
+#GET 요청이 오면 함수를 호출
+@api_view(['GET'])
+def hello(request):
+    return Response("Hello REST API")
--- apiproject/urls.py
+++ apiproject/urls.py
@@ -1,22 +1,12 @@
-"""
-URL configuration for apiproject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
+#url의 처리를 다른 모듈에게 위임하고자 할 때 사용하는 패키지를 import
+from django.urls import include
+
+#path("example/", include("apiapp.urls")), 문장은
+#example로 시작하는 요청이 오면 apiapp 애플리케이션의 urls.py 파일에 처리를 위임
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("example/", include("apiapp.urls")),
 ]
