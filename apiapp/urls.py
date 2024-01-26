fbc985743b0ccae82fde7cce048c7be3b4747385
adam <itstudy@kakao.com>
2024-01-26 16:31:25 +0900

    삽입과 전체보기 추가

--- apiapp/urls.py
+++ apiapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import hello
+from .views import hello, booksAPI
 
 urlpatterns = [
     path("hello/", hello),
+    path("books/", booksAPI),
 ]
--- apiapp/views.py
+++ apiapp/views.py
@@ -9,3 +9,32 @@ from rest_framework.decorators import api_view
 @api_view(['GET'])
 def hello(request):
     return Response("Hello REST API")
+
+from rest_framework import status
+from rest_framework.generics import get_object_or_404
+
+from .models import Book
+from .serializers import BookSerializer
+
+#GET 과 POST 모두를 처리
+@api_view(['GET', 'POST'])
+def booksAPI(request):
+    #GET 방식의 처리 - 전체 조회를 요청하는 경우
+    if request.method == 'GET':
+        #테이블의 데이터를 전부 가져오기
+        books = Book.objects.all()
+        #출력하기 위해서 브라우저의 형식으로 데이터를 변환
+        serializer = BookSerializer(books, many=True)
+        #출력
+        return Response(serializer.data)
+    #POST 방식의 처리 - 삽입하는 경우
+    elif request.method == "POST":
+        #클라이언트에서 전송된 데이터를 가지고 Model 인스턴스 생성
+        serializer = BookSerializer(data = request.data)
+        #유효성 검사를 수행해서 통과하면 삽입하고 그렇지 않으면 
+        #실패한 이유를 출력합니다.
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+                 
